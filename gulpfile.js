9514d896912393485409bf8afd381df45dd5e759
Alan Santos <asantos@assessor.lacounty.gov>
2016-08-02 15:41:16 -0700

    Revise gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,71 @@
 
 var gulp = require('gulp');
 var del = require('del');
+var runSequence = require('run-sequence');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ejs = require('gulp-ejs');
+var ngConfig = require('gulp-ng-config');
 
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 
-var bcd = './bower_components'; // Bower Components Directory
+var build = (process.env.NODE_ENV || 'Debug').trim();
+var config = require('./config.json')[build];
+config.build = build;
+console.log('Building ' + config.build + ' Environment');
+config.version = require('./package.json').version;
+config.bowerFiles = require('./config.json').bowerFiles;
+config.jsFiles = [];
+config.cssFiles = [];
 
-gulp.task('default', ['compileWatch', 'browserSync']);
+var bcd = function(filepath) {
+	return './bower_components' + (filepath || '');
+};
+var lib = function(filepath) {
+	return 'lib' + (filepath || '');
+};
+
+gulp.task('default', [config.build]);
+
+gulp.task('Debug', function(cb) {
+	Array.prototype.push.apply(config.jsFiles, config.bowerFiles['js'].map(lib));
+	Array.prototype.push.apply(config.cssFiles, config.bowerFiles['css'].map(lib));
+	config.jsFiles.push('js/app.js');
+	config.jsFiles.push('js/config.js');
+	runSequence('clean', ['bowerComponents', 'compile', 'watch'], 'browserSync', cb);
+});
 
 // Clean
-var cleaned = false;
 gulp.task('clean', function(cb) {
-	if (!cleaned) {
-		console.log('cleaning...');
-		del(['./public/**']).then(function() {
-			cleaned = true;
-			console.log('cleaned!');
-		}).then(cb);
-	}
+	return del(['./public']);
 });
 
 // Copy bower components
-gulp.task('bowerComponents', ['clean'], function() {
-	return gulp.src([bcd + '/angular/angular.min.js'], {
-			base: bcd
+gulp.task('bowerComponents', function() {
+	var components = [];
+	for (var filetype in config.bowerFiles) {
+		components = components.concat(config.bowerFiles[filetype]);
+	}
+	return gulp.src(components.map(bcd), {
+			base: bcd()
 		})
 		.pipe(gulp.dest('./public/lib'));
 });
 
-// Compile Ejs templates
-gulp.task('compileIndex', ['clean'], function() {
+gulp.task('compile', ['compileIndex', 'compileJavascripts', 'compileConfig']);
+
+// Compile Index page
+gulp.task('compileIndex', function() {
 	return gulp.src('./source/index.ejs')
-		.pipe(ejs(null, {
+		.pipe(ejs(config, {
 			ext: '.html'
 		}))
 		.pipe(gulp.dest('./public'));
 });
 
-// Compile Javascripts: Concat files and mini/Uglify
+// Compile Javascripts: Concat files
 gulp.task('compileJavascripts', ['clean'], function() {
 	return gulp.src(['./source/js/**/app.*.js', // order is important
 			'./source/js/**/service.*.js',
@@ -52,14 +75,21 @@ gulp.task('compileJavascripts', ['clean'], function() {
 			'./source/js/**/controller.*.js'
 		])
 		.pipe(concat('app.js'))
-		.pipe(gulp.dest('./public/js'))
-		.pipe(rename('app.min.js'))
-		.pipe(uglify())
 		.pipe(gulp.dest('./public/js'));
 });
-gulp.task('compile', ['bowerComponents', 'compileIndex', 'compileJavascripts']);
 
-gulp.task('compileWatch', ['compile'], function() {
+// Compile config file for angular
+gulp.task('compileConfig', function() {
+	return gulp.src('./config.json')
+		.pipe(ngConfig('neat', {
+			createModule: false,
+			environment: config.build,
+		}))
+		.pipe(gulp.dest('./public/js'));
+});
+
+// set Watch
+gulp.task('watch', ['compile'], function() {
 	gulp.watch(['./source/js/**/*.js'], ['compileJavascripts']);
 	gulp.watch(['./source/index.ejs'], ['compileIndex']);
 });
